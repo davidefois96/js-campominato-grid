a69c45fabd097b516436ba815ea0dbf7a09df568
davidefois96 <davidefois360@gmail.com>
2024-02-29 17:47:24 +0100

    [add] bonus

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,84 @@
  
 const myGrid = document.querySelector('.myGrid');
-
 const btn = document.querySelector('button')
+const sel = document.querySelector('select')
 
 
 
 console.log(btn);
+alert(' Benvenuto ! per giocare premi gioca!');
 
 btn.addEventListener('click', function(){
 
   reset();
 
-  for (let i = 1; i <= 100; i++) {
-    const box = addInteractiveSquare(i);
+  alert('scegli la difficoltà ');
 
-    box.classList.add('d-flex');
+  sel.addEventListener('click', function(){
 
-    box.innerHTML= i;
+    reset();
+
+
+    if (sel.value==='facile') {
+
+      for (let i = 1; i <= 100; i++) {
+        const box = addInteractiveSquare(i);
     
-    myGrid.append(box);
+        box.classList.add('d-flex');
+        box.classList.add('square');
+        box.classList.add('easy');
+  
+        
     
- 
-  }
+        box.innerHTML= i;
+        
+        myGrid.append(box);
+        
+     
+      }
+    
+      
+      
+    } else if (sel.value==='medio') { 
+  
+      for (let i = 1; i <= 81; i++) {
+        const box = addInteractiveSquare(i);
+    
+        box.classList.add('d-flex');
+        box.classList.add('square');
+        box.classList.add('middle');
+  
+    
+        box.innerHTML= i;
+        
+        
+        myGrid.append(box);
+      }
+     
+        
+    } else if (sel.value==='difficile') {
+  
+      for (let i = 1; i <= 49; i++) {
+        const box = addInteractiveSquare(i);
+    
+        box.classList.add('d-flex');
+        box.classList.add('square');
+        box.classList.add('difficult');
+  
+    
+        box.innerHTML= i;
+        
+        
+        myGrid.append(box);
+      }
+  
+  
+    } 
+      
+  
+
+  })
+  
 
 
 })
@@ -30,7 +87,7 @@ function addInteractiveSquare(num){
 
   const square= document.createElement('div');
 
-  square.className= 'square';
+  
 
   square._id= num;
 
